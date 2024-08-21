4a8805f20a4487624ebf1104ab3eda903f4e4f8e
ShreddShark <daniel.cox2685@gmail.com>
2024-08-21 18:33:39 -0500

    added sample images for merch

--- src/components/sub-components/Footer.jsx
+++ src/components/sub-components/Footer.jsx
@@ -2,14 +2,14 @@ const Footer = () => {
   return (
     <>
       <section id="footer">
-        <footer className="relative w-full bg-black text-white p-4 text-center">
+        <footer className="relative w-full p-4 text-center text-white bg-black">
           <p>
             &copy; {new Date().getFullYear()} Jawfane Band. All rights reserved.
           </p>
         </footer>
       </section>
     </>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
